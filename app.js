5ae7f47ee48313e0ae165f82025ec2430cf31ba7
Jason Varghese <varghe0@unbc.ca>
2020-04-01 17:46:14 -0700

    add gitignore,procfile

--- app.js
+++ app.js
@@ -217,6 +217,10 @@ app.post("/weathermap", function getViewType(req, res) {
 
 })
 
+let port = process.env.PORT;
+if(port== null|| port ==""){
+  port=3000;
+}
 app.listen(process.env.PORT||3000, function() {
   console.log("running");
 })
