4e8b2516fd4a414a6ffd87d80af815cadc242112
Jason Varghese <varghe0@unbc.ca>
2020-04-07 20:52:45 -0700

    added remaining files

--- app.js
+++ app.js
@@ -221,7 +221,7 @@ let port = process.env.PORT;
 if(port== null|| port ==""){
   port=3000;
 }
-app.listen(process.env.PORT||3000, function() {
+app.listen(port,function() {
   console.log("running");
 })
 // app.listen(4000, function() {
